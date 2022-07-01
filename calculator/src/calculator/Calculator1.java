1ed233793d2f14b84e48d80c3444791517ab0de3
lakshmankumar <laxmansgvp@gmail.com>
2022-07-01 19:11:53 +0530

    calculator

---
+++ calculator/src/calculator/Calculator1.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator1{
+	public double add(double num1, double num2) {
+		return num1+num2;
+	}
+	public static void main(String[] args) {
+		calculator1 calculator1=new calculator1();
+		double result=calculator.add(12.5,14.7);
+		System.out.println(result);
+	}
+
+}
